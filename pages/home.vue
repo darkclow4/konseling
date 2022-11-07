366fe65d09906016ffb7bd8890fbae06b6426bdd
darkclow4 <irfan.140895@gmail.com>
2022-11-08 05:59:28 +0700

    add some layout

--- pages/home.vue
+++ pages/home.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="flex bg-blue-100 grow">
-        
+    <div class="flex grow p-5">
+        <div class="flex flex-col basis-2/3">
+            <div class="flex basis-1/3 bg-white">
+                <div class="aspect-square bg-gray-200 mr-5"></div>
+                <div class="flex-auto bg-black"></div>
+            </div>
+            <div class="flex basis-2/3 bg-blue-200 mt-5"></div>
+        </div>
+        <div class="flex bg-yellow-400 ml-5 basis-1/3">
+        </div>
     </div>
 </template>
 
@@ -11,5 +19,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
